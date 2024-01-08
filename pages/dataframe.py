a74cabba7b107ae480f6995bad8b8bc0dc24a709
ldmfabio <ldmfabio@gmail.com>
2024-01-08 18:16:19 -0300

    Aula 15

--- pages/dataframe.py
+++ pages/dataframe.py
@@ -1,5 +1,30 @@
 import streamlit as st
 from dataset import df
 
-st.title('Dataset de Vendas')
+st.title("Dataset de Vendas")
+with st.expander("Colunas"):
+    colunas = st.multiselect("Selecione as Colunas", list(df.columns), list(df.columns))
+st.sidebar.title("Filtros")
+with st.sidebar.expander("Categoria do Produto"):
+    categorias = st.multiselect(
+        "Selecione as categgrias",
+        df["Categoria do Produto"].unique(),
+        df["Categoria do Produto"].unique(),
+    )
+
+with st.sidebar.expander("Preço do Produto"):
+    preco = st.slider(
+        "Selecione o intervalo de preço",
+        df["Preço"].min(),
+        df["Preço"].max(),
+        (df["Preço"].min(), df["Preço"].max())
+        # 0, 5000,
+        # (0, 5000)
+    )
+
+with st.sidebar.expander("Data da Compra"):
+    data_compra = st.date_input(
+        "Selecione a data",
+        (df["Data da Compra"].min(), df["Data da Compra"].max()),
+    )
 st.dataframe(df)
